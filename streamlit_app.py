8a38b963eb5839d380966364181a0ea8cd43a658
AkhilaSE <110368100+AkhilaSE@users.noreply.github.com>
2023-12-12 10:46:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 streamlit.title('My parents Healthy Diner')
 streamlit.header('Breakfast Favourites')
-streamlit.text(🥣'Omega 3 & Blue berry oatmeal')
-streamlit.text(🥗'kale spinach & rocket smoothie')
-streamlit.text(🐔'Hard boiled Free-rane egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣Omega 3 & Blue berry oatmeal')
+streamlit.text('🥗kale spinach & rocket smoothie')
+streamlit.text('🐔Hard boiled Free-rane egg')
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
